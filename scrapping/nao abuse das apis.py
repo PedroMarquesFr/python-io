3e5c04a16d11c87409951b4b2fcbd55d441fe7a6
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-04-26 18:47:49 -0300

    adicionado mudancas no rep

---
+++ scrapping/nao abuse das apis.py
@@ -0,0 +1,20 @@
+import requests
+import time
+
+
+# Coloca uma pausa de 6 segundos a cada requisição
+# Obs: este site de exemplo tem um rate limit de 10 requisições por minuto
+for _ in range(15):
+    response = requests.get("https://www.cloudflare.com/rate-limit-test/")
+    print(response.status_code)
+    time.sleep(6) # o timeoutresolve o problema
+
+
+try:
+    resp = requests.get("http://httpbin.org/delay/10", timeout=2)
+except requests.Timeout:
+    resp = requests.get("http://httpbin.org/delay/1", timeout=2)
+finally:
+    
+    print(resp.status_code)
+
